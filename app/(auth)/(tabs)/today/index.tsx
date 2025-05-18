94be8916f9d95f72b219a08a54ecea868afec4fe
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-05-18 22:18:35 +0545

    wip sectionlist

--- app/(auth)/(tabs)/today/index.tsx
+++ app/(auth)/(tabs)/today/index.tsx
@@ -1,17 +1,47 @@
 import Fab from '@/components/Fab';
-import { todos } from '@/db/schema';
+import { projects, todos } from '@/db/schema';
+import { Todo } from '@/types/interface';
+import { format } from 'date-fns';
+import { eq } from 'drizzle-orm';
 import { drizzle, useLiveQuery } from 'drizzle-orm/expo-sqlite';
 import { useDrizzleStudio } from 'expo-drizzle-studio-plugin';
 import { useSQLiteContext } from 'expo-sqlite';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ScrollView, Text, View } from 'react-native';
+interface Section {
+  title: string;
+  data: Todo[];
+}
+
 
 export default function index() {
   const db = useSQLiteContext();
   const drizzleDb = drizzle(db);
   useDrizzleStudio(db);
-  const { data } = useLiveQuery(drizzleDb.select().from(todos));
+  const { data } = useLiveQuery(drizzleDb.select()
+  .from(todos)
+  .leftJoin(projects, eq(todos.project_id, projects.id))
+  .where(eq(todos.completed, 0)));
   console.log("data", data);
+
+  useEffect(() => {
+    const formatedData = data?.map((item) => ({
+      ...item.todos,
+      project_name: item.projects?.name,
+      project_color: item.projects?.color,
+    }))
+
+    const groupedByDay = formatedData?.reduce((acc: { [key: string]: Todo[] }, task) => {
+      const day = format(new Date(task.due_date || new Date()), 'd MMM · eee');
+      if (!acc[day]) {
+        acc[day] = [];
+      }
+      acc[day].push(task);
+      return acc;
+    }, {});
+  
+  
+  })
 
   return (
     <View style={{ flex: 1, backgroundColor: '#fff', paddingTop: 60 }}>
